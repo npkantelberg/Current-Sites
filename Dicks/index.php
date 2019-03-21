8f1469b36e77bafd7671696de9749c8002a2661c
Nicholas Kantelberg <nicholaskantelberg@Nicholass-MacBook-Pro.local>
2019-03-21 18:59:49 -0500

    Started the homepage HTML

--- Dicks/index.php
+++ Dicks/index.php
@@ -2,9 +2,10 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Roboto" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 
 <title>Dicks Drive In Kaukauna</title>
@@ -14,7 +15,9 @@
 </section>
 
 <body>
-
+<div class="container">
+	
+</div>
 </body>
 
 
