89a6819fe244e2982f9825044f7380ce79b28bfe
Nicholas Kantelberg <nicholaskantelberg@Nicholass-MacBook-Pro.local>
2019-04-10 20:47:25 -0500

    a bunch of updates

--- Dicks/index.php
+++ Dicks/index.php
@@ -41,21 +41,41 @@
 
 			</div>
 			<div class="page about-page">
-				About Page
 				<div class="clickable menu-toggle">
 					<span class="delimiter"></span>
 					<span class="delimiter"></span>
 					<span class="delimiter"></span>
 					<span class="delimiter"></span>
 				</div>
+				<div class="about-page-inner">
+					<div class="about-page-header">
+						<div class="about-page-hero">
+
+						</div>
+					</div>
+					<div class="body-wrapper">
+						<div class="about-content">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit ipsum vel justo consequat fermentum. Nam pharetra neque a varius vulputate. Maecenas sit amet ullamcorper ante. Nunc nec imperdiet orci. In enim eros, lobortis in enim et, viverra eleifend tortor. Integer ultricies augue et auctor elementum. Nullam aliquet venenatis pulvinar. Etiam nec viverra mauris, sed dignissim risus. Aliquam erat volutpat. Nullam vitae sem at libero vestibulum dapibus. Nulla finibus tempor ipsum a suscipit. Aenean lectus dui, mollis a felis id, tincidunt aliquet lectus. Fusce in sodales nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et ex mi.
+							</p>
+							<p>
+								Sed consectetur laoreet dui, quis ullamcorper leo congue vel. Pellentesque laoreet blandit fringilla. Nam feugiat rhoncus pellentesque. Pellentesque eros neque, porttitor eget massa in, mollis cursus nisi. In lorem lacus, condimentum vel sem et, luctus ullamcorper leo. Donec quis dolor ligula. Pellentesque et mattis dui. </p>
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div class="page apply-page">
-				Apply Page
 				<div class="clickable menu-toggle">
 					<span class="delimiter"></span>
 					<span class="delimiter"></span>
 					<span class="delimiter"></span>
 					<span class="delimiter"></span>
+				</div>
+				<div class="application-page-inner">
+					<div class="application">
+						<iframe allowTransparency="true" style="min-height:1000px; height:inherit; overflow:auto;" width="100%" id="contactform123" name="contactform123" marginwidth="0" marginheight="0" frameborder="0" src="//www.123formbuilder.com/my-contact-form-4692620.html"><p>Your browser does not support iframes. The contact form cannot be displayed. Please use another contact method (phone, fax etc)</p></iframe>
+					</div>
 				</div>
 			</div>
 			<div class="page contact-page">
